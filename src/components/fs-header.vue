bbc20bbe721263b0378ba7b7ac3df928a53fdd94
huangbinyuan <1024209814@qq.com>
2022-02-24 15:57:16 +0800

    feat: mobile

--- src/components/fs-header.vue
+++ src/components/fs-header.vue
@@ -6,10 +6,51 @@
     <PageContainer class="fs-flex-ic fs-h-16">
       <FsLogo />
       <span class="fs-flex-1"></span>
-      <div class="wrapper fs-h-full">
+      <div v-if="notPhone" class="wrapper fs-h-full">
         <div class="inner-wrapper fs-w-full fs-h-full fs-justify-end fs-flex">
-          <FsMenu />
+          <FsMenu
+            mode="horizontal"
+            style="
+              width: 100%;
+              background: transparent;
+              display: flex;
+              align-items: center;
+              border-bottom: none;
+            "
+          />
         </div>
+      </div>
+
+      <div v-else @click="handleOpenMenu">
+        <ADrawer
+          class="menu-drawer"
+          :bodyStyle="{ padding: 0 }"
+          :zIndex="999"
+          :title="null"
+          :placement="'left'"
+          :visible="isVisible"
+        >
+          <FsMenu class="fs-mt-20" mode="inline" @select="handleSelect" />
+        </ADrawer>
+        <svg
+          class="icon"
+          style="
+            width: 1.5em;
+            height: 1.5em;
+            vertical-align: middle;
+            fill: currentColor;
+            overflow: hidden;
+          "
+          viewBox="0 0 1024 1024"
+          version="1.1"
+          xmlns="http://www.w3.org/2000/svg"
+          p-id="1453"
+        >
+          <path
+            d="M963.764706 180.705882v120.470589H60.235294V180.705882h903.529412zM60.235294 602.352941h903.529412V481.882353H60.235294v120.470588z m0 301.176471h903.529412v-120.470588H60.235294v120.470588z"
+            p-id="1454"
+          ></path>
+        </svg>
       </div>
     </PageContainer>
   </header>
@@ -19,10 +60,13 @@
 import FsLogo from './fs-logo.vue'
 import FsMenu from './fs-menu/fs-menu.vue'
 import PageContainer from './container/page-container.vue'
-import { computed } from 'vue'
+import { computed, ref } from 'vue'
+import { useBreakpoints, breakpointsTailwind } from '@vueuse/core'
 import { useScrollContext } from '@/context'
 
 const { isTop } = useScrollContext()
+const bps = useBreakpoints(breakpointsTailwind)
+const notPhone = bps.sm
 
 const headerStyle = computed(() => {
   const top = isTop.value
@@ -31,6 +75,22 @@ const headerStyle = computed(() => {
     background: top ? 'var(--color-banner-bg)' : 'white',
   }
 })
+
+const isVisible = ref(false)
+
+const handleOpenMenu = () => {
+  isVisible.value = !isVisible.value
+}
+
+const handleSelect = () => {
+  isVisible.value = false
+}
 </script>
 
-<style scoped></style>
+<style lang="scss" scoped>
+.menu-drawer {
+  .ant-drawer-body {
+    padding: 0;
+  }
+}
+</style>
